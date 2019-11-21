098c311c82cbaaad68fe915ad1dc49b5207a7c5a
luanshiyinyang <1695735420@qq.com>
2019-11-21 09:20:33 +0800

    add sh script

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,6 +1,6 @@
-wget https://ndownloader.figshare.com/files/12855005
-apt install unrar
-mv 12855005 data.rar
-unrar x data.rar ./
-rm -rf Caltech101/*/*.fig
-rm -rf Caltech101/*/*.bmp
+`wget https://ndownloader.figshare.com/files/12855005`
+`mv 12855005 data.rar`
+`apt install unrar`
+`unrar x data.rar ./`
+`rm -rf Caltech101/*/*.fig`
+`rm -rf Caltech101/*/*.bmp`
